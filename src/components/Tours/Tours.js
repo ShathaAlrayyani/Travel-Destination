3f6681c15e0d1d9506aa90c320898c33faa67afb
Shatha Alrayyani <shatha.alrayyani223@gmail.com>
2022-05-15 21:09:57 +0300

    saving before creating new branch

--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -3,12 +3,11 @@ import '../Tours/Tours.css';
 function Tour (props){
 
     return (
+        
         <div>
 
             <p className="title"> {props.name}</p>
             <img className="img" src= {props.img} alt = {props.alt} />
-            <br/>
-            <br/>
             <br/>
             <br/>
             
